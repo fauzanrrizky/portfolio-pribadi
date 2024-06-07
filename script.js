714334aac4d30a54d8431724632da66649d50b73
fauzanrrizky <mfauzan.rizky@gmail.com>
2024-06-07 18:22:11 +0700

    update web content

--- script.js
+++ script.js
@@ -29,4 +29,43 @@ function closeModal() {
   var modal = document.getElementById("myModal");
   modal.style.display = "none";
 }
-  
+
+
+const texts = ["Data Analytics", "Data Engineering", "Machine Learning", "Data Science"];
+let index = 0;
+let textIndex = 0;
+let typingSpeed = 40;
+
+function typeText(element, text, callback) {
+    if (textIndex < text.length) {
+        element.innerHTML += text.charAt(textIndex);
+        textIndex++;
+        setTimeout(() => typeText(element, text, callback), typingSpeed);
+    } else {
+        setTimeout(callback, 600);
+    }
+}
+
+function deleteText(element, callback) {
+    if (textIndex > 0) {
+        element.innerHTML = element.innerHTML.substring(0, textIndex - 1);
+        textIndex--;
+        setTimeout(() => deleteText(element, callback), typingSpeed);
+    } else {
+        callback();
+    }
+}
+
+function changeText() {
+    const textElement = document.getElementById("changing-interest");
+
+    deleteText(textElement, () => {
+        textIndex = 0;
+        typeText(textElement, texts[index], () => {
+            index = (index + 1) % texts.length;
+            setTimeout(changeText, 600);
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", changeText);
